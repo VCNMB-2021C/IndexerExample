c593a36c0311923471be510903ab33c8683dcb68
rwanv <rwanv@REECE-XPS>
2021-08-04 15:15:11 +0200

    Add project files.

---
+++ IndexerExample/Employee.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerExample
+{
+    class Employee
+    {
+        double salary;
+        string empNumber, jobTitle, name, surname;
+
+        public Employee(string empNumber, string jobTitle,
+            string name, string surname,double salary )
+        {
+            this.salary = salary;
+            this.empNumber = empNumber;
+            this.jobTitle = jobTitle;
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public double Salary
+        {
+            get
+            {
+                return Salary;
+            }
+            set
+            {
+                this.salary = value;
+            }
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return this.empNumber;
+                else if (index == 1)
+                    return this.jobTitle;
+                else if (index == 2)
+                    return this.name;
+                else if (index == 3)
+                    return this.surname;
+                else if (index == 4)
+                    return this.salary;
+                return null;
+            }
+            set
+            {
+                if (index == 0)
+                    this.empNumber=(string)value;
+                else if (index == 1)
+                    this.jobTitle = (string)value;
+                else if (index == 2)
+                    this.name = (string)value;
+                else if (index == 3)
+                    this.surname = (string)value;
+                else if (index == 4)
+                    this.salary = (double)value;
+            }
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName.ToLower().Equals("empnumber"))
+                    return this.empNumber;
+                else if (attrName.ToLower().Equals("jobtitle"))
+                    return this.jobTitle;
+                else if (attrName.ToLower().Equals("name"))
+                    return this.name;
+                else if (attrName.ToLower().Equals("surname"))
+                    return this.surname;
+                else if (attrName.ToLower().Equals("salary"))
+                    return this.salary;
+                return null;
+            }
+            set
+            {
+                if (attrName.ToLower().Equals("empnumber"))
+                        this.empNumber = (string)value;
+                else if (attrName.ToLower().Equals("jobtitle"))
+                    this.jobTitle = (string)value;
+                else if (attrName.ToLower().Equals("name"))
+                    this.name = (string)value;
+                else if (attrName.ToLower().Equals("surname"))
+                    this.surname = (string)value;
+                else if (attrName.ToLower().Equals("salary"))
+                    this.salary = (double)value;
+            }
+        }
+
+    }
+}
---
+++ IndexerExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IndexerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("EMP0111", "Director", "Bob",
+                "Tim", 8000);
+            emp[2] = "Bobby";
+            Console.WriteLine(emp[0]);
+            Console.WriteLine(emp[1]);
+            Console.WriteLine(emp[2]);
+            Console.WriteLine(emp[3]);
+            Console.WriteLine(emp[4]);
+            emp["salary"] = 80000;
+            Console.WriteLine("Employee Number :" + emp["empNumber"]);
+            Console.WriteLine("Employee Title :" + emp["jobTitle"]);
+            Console.WriteLine("Employee Name :" + emp["name"]);
+            Console.WriteLine("Employee Surname :" + emp["surname"]);
+            Console.WriteLine("Employee Salary :" + emp["salary"]);
+        }
+    }
+}
